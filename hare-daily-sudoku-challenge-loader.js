a4db483c1473cc0bcb0605ef107a9209cac000b7
harepublishing <info@harepublishing.com>
2026-05-08 08:54:59 -0400

    Update hare-daily-sudoku-challenge-loader.js

--- hare-daily-sudoku-challenge-loader.js
+++ hare-daily-sudoku-challenge-loader.js
@@ -1,6 +1,6 @@
 (() => {
-  const ENGINE_URL =
-    "https://cdn.jsdelivr.net/gh/harepublishing/harepublishing-puzzles@main/hare-daily-sudoku-challenge-engine.js";
+const ENGINE_URL =
+  "https://cdn.jsdelivr.net/gh/harepublishing/harepublishing-puzzles@main/hare-daily-sudoku-challenge-engine.js?v=2026-05-08-2";
 
   const existing = document.querySelector('script[data-hp-dsc-engine="true"]');
   if (existing) return;
